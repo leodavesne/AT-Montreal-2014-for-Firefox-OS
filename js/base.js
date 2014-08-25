fee2675a45db92fbe3ca54fac0b2cad05b1b0953
Léo Davesne <leo@davesne.net>
2014-08-25 16:09:53 -0400

    Update base.js

--- js/base.js
+++ js/base.js
@@ -1,1 +1,9 @@
-fsadf
+// General JavaScript file
+
+var canInstall = !!(navigator.mozApps && navigator.mozApps. installPackage);
+
+if (canInstall) {
+  var manifestUrl = location.href.substring(0,     
+  location.href.lastIndexOf("/")) + "/mypackage.webapp";
+  var installApp = navigator.mozApps.installPackage(manifestUrl);
+}
