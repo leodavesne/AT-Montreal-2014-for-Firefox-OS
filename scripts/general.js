f5013a19bd0d6d564619e7934e06f1e7396c113c
Léo Davesne <leo@davesne.net>
2014-08-25 15:32:33 -0400

    Update general.js

--- scripts/general.js
+++ scripts/general.js
@@ -1,1 +1,10 @@
 // General JavaScript file
+
+var canInstall = !!(navigator.mozApps && navigator.mozApps. installPackage);
+
+
+if (canInstall) {
+  var manifestURL = location.href.substring(0,     
+  location.href.lastIndexOf("/")) + "/mypackage.webapp";
+  var installApp = navigator.mozApps.installPackage(manifestURL);
+}
